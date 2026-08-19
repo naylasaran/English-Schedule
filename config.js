e5043fd9eb15aaeb809d180a14bcc321b771973e
naylasaran <naylac.saran@gmail.com>
2026-08-18 22:24:42 -0300

    config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://oqvwovwezewoxwfkdeph.supabase.co";
+const SUPABASE_KEY = "sb_publishable_ntVAIZjDLkAuqkz2nQ5aIQ_RcHJMc5z";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
